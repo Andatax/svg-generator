3e05ac2c54063a811170bacff5a1a3c8fc102156
Andatax <abraham.mendez1011@gmail.com>
2023-11-14 02:47:59 -0600

    Added test files for shapes and js files for shapes classes

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const Rectangle = require("./lib/rectangle");
+const Circle = require("./lib/circle");
+const Square = require("./lib/square");
+inquirer
+	.prompt([
+		{
+			type: "list",
+			name: "shapeSelection",
+			message: "Select a shape:",
+			choices: ["square", "rectangle", "circle"],
+		},
+		{
+			type: "input",
+			name: "color",
+			message: "Enter the color for the shape (e.g., #ff0000 or red):",
+			default: "#000000",
+		},
+	])
+	.then(shapeSelected => {
+		const shapePrompts = [];
+
+		if (shapeSelected.shapeType === "rectangle") {
+			shapePrompts.push({
+				type: "number",
+				name: "width",
+				message: "Enter width for the rectangle:",
+			});
+			shapePrompts.push({
+				type: "number",
+				name: "height",
+				message: "Enter height for the rectangle:",
+			});
+		} else if (shapeSelected.shapeType === "circle") {
+			shapePrompts.push({
+				type: "number",
+				name: "radius",
+				message: "Enter radius for the circle:",
+			});
+		} else if (shapeSelected.shapeType === "square") {
+			shapePrompts.push({
+				type: "number",
+				name: "width",
+				message: "Enter width for the square:",
+			});
+		}
+	})
+	.catch(error => {
+		console.log(error);
+	});
